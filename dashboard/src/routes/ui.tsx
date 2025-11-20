5fa10c295f1d56db1b25d9d8c2d6e5b6902fb9b9
fresonn <vladislavs319@gmail.com>
2025-11-20 09:44:45 +0200

    web: chore(ui): add custom fonts

--- dashboard/src/routes/ui.tsx
+++ dashboard/src/routes/ui.tsx
@@ -30,7 +30,7 @@ function RouteComponent() {
       <Button variant="secondary">Click me!</Button>
       <Button variant="ghost">Click me!</Button>
       <Button variant="link">Click me!</Button>
-      <div className="mt-10">
+      <div className="my-10">
         <Button loading={loading} onClick={hanleClick}>
           <Database />
           Loading
@@ -44,6 +44,19 @@ function RouteComponent() {
           <Input placeholder="Port" type="number" />
           <Input disabled placeholder="Port" type="number" />
         </div>
+      </div>
+      <div>
+        <p className="font-code">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero consequuntur voluptatibus
+          laudantium velit nam deserunt quis perspiciatis quaerat voluptas voluptatum labore culpa
+          ex nulla dignissimos incidunt, dolor architecto, praesentium ipsam, molestiae nesciunt
+          minima quasi ut sit tenetur! Tenetur maxime enim explicabo iure quis aperiam deleniti ab?
+          Natus cum a, animi reprehenderit ipsa est magnam numquam mollitia aliquam placeat officia,
+          inventore quam alias nam incidunt. Reiciendis nostrum ad alias laborum dolores. Et unde
+          corrupti non ratione nulla aperiam eum. Aut quidem, nihil ut nemo temporibus laboriosam
+          corporis? Aliquid exercitationem neque velit magni necessitatibus nemo placeat ullam
+          minima. Dignissimos aspernatur quis sit.
+        </p>
       </div>
     </div>
   )
--- dashboard/tailwind.config.ts
+++ dashboard/tailwind.config.ts
@@ -4,7 +4,15 @@ const config: Config = {
   future: {
     hoverOnlyWhenSupported: true
   },
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}']
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Helvetica Neue', 'Arial', 'Menlo', 'Consolas', 'sans-serif'],
+        code: ['Fira Code', 'Menlo', 'Consolas', 'monospace']
+      }
+    }
+  }
 }
 
 export default config

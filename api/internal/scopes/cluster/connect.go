d5343788a1befe6001f082e4a5a6bb73f95edd4e
fresonn <vladislavs319@gmail.com>
2025-12-04 14:54:58 +0200

    api: fix(oapi): connection database now optional

--- api/internal/scopes/cluster/connect.go
+++ api/internal/scopes/cluster/connect.go
@@ -11,6 +11,8 @@ func (c *Cluster) Connect(ctx context.Context, authData entities.AuthData) error
 
 	c.logger.Info("try to establish postgres connection")
 
+	authData.PrettyLog()
+
 	if ok := c.pgManager.IsConnected(); ok {
 		c.logger.WarnContext(ctx, "connection already established")
 		return errors.New("connection already established")
@@ -22,7 +24,21 @@ func (c *Cluster) Connect(ctx context.Context, authData entities.AuthData) error
 		return err
 	}
 
-	err = c.pgManager.UpdateConnection(ctx, config.Connection(authData))
+	conn := config.Connection{
+		Host:     authData.Host,
+		Port:     authData.Port,
+		User:     authData.User,
+		Password: authData.Password,
+		SSLMode:  authData.SSLMode,
+	}
+
+	if authData.Database != nil {
+		conn.Database = *authData.Database
+	} else {
+		conn.Database = "postgres"
+	}
+
+	err = c.pgManager.UpdateConnection(ctx, conn)
 	if err != nil {
 		c.logger.Error("set connection", "error", err)
 		return err
